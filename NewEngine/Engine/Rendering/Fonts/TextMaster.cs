944eb9ec038a07fa8d2b6eb2807d6da9349a948f
Mathias Danielsen <mathais234@gmail.com>
2017-05-04 22:27:34 +0200

    Removed text master

--- NewEngine/Engine/Rendering/Fonts/TextMaster.cs
+++ NewEngine/Engine/Rendering/Fonts/TextMaster.cs
@@ -11,56 +11,6 @@ using OpenTK.Graphics.OpenGL;
 
 namespace NewEngine.Engine.components {
     public class TextMaster {
-        private int _vao;
-        private static Dictionary<FontType, List<GUIText>> _texts = new Dictionary<FontType, List<GUIText>>();
 
-        public static void LoadText(GUIText text) {
-            FontType font = text.Font;
-            TextMeshData data = font.LoadText(text);
-            int vao = LoadToVAO(data.VertexPositions, data.TextureCoords);
-            text.SetMeshInfo(vao, data.VertexCount);
-
-            List<GUIText> textBatch = null;
-            if (_texts.ContainsKey(font))
-                textBatch = _texts[font];
-
-            if (textBatch == null) {
-                textBatch = new List<GUIText>();
-                _texts.Add(font, textBatch);
-            }
-
-            textBatch.Add(text);
-        }
-
-        public static void RemoveText(GUIText text) {
-            List<GUIText> textBatch = _texts[text.Font];
-            textBatch.Remove((text));
-            if (textBatch.Count <= 0) {
-                _texts.Remove(text.Font);
-            }
-        }
-
-        public static int LoadToVAO(float[] positions, float[] textureCoords) {
-            int vaoId;
-            GL.GenVertexArrays(1, out vaoId);
-            GL.BindVertexArray(vaoId);
-
-            StoreDataInAttrib(0, 2, positions);
-            StoreDataInAttrib(1, 2, textureCoords);
-
-            GL.BindVertexArray(0);
-
-            return vaoId;
-        }
-
-        private static void StoreDataInAttrib(int attributeNumber, int coordinateSize, float[] data) {
-            int vboId;
-            GL.GenBuffers(1, out vboId);
-
-            GL.BindBuffer(BufferTarget.ArrayBuffer, vboId);
-            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(data.Length * 4), data, BufferUsageHint.StaticDraw);
-            GL.VertexAttribPointer(attributeNumber, coordinateSize, VertexAttribPointerType.Float, false, 0, 0);
-            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-        }
     }
 }
